76877a435222b1bfaf1369d933bed8e98b97cc9e
Magdalena Grad <mgdln.grad@gmail.com>
2019-12-18 20:36:13 +0100

    innitial commit

---
+++ src/components/Posts.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Posts:</h1>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
