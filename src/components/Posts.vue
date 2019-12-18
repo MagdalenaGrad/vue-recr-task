e1cbf28d94461e8ebbc3cc23f0019a5399a30619
Magdalena Grad <mgdln.grad@gmail.com>
2019-12-18 21:26:04 +0100

    get data

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,18 +1,48 @@
 <template>
   <div>
+    <h1>Users:</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id" v-text="user.name"></li>
+    </ul>
     <h1>Posts:</h1>
     <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
+      <li v-for="post in posts" :key="post.id" v-text="post.body"></li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+window.axios = require("axios");
+
+export default {
+  methods: {
+    getPosts: async function() {
+      await axios
+        .get("https://jsonplaceholder.typicode.com/posts")
+        .then(res => {
+          console.log(res);
+          this.posts = res.data;
+        });
+    },
+    getUsers: function() {
+      axios.get("https://jsonplaceholder.typicode.com/users").then(res => {
+        console.log(res);
+        this.users = res.data;
+      });
+    }
+  },
+  mounted: function() {
+    this.getUsers();
+    this.getPosts();
+  },
+  data: function() {
+    return {
+      posts: null,
+      users: null
+    };
+  }
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import Vue from "vue";
-// import axios from 'axios';
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
-
-
 
 new Vue({
   render: h => h(App)
